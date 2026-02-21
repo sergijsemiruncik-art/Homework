2ea89a5f8e6e43d79bbac53f9250620fe8d1f6d7
sergijsemiruncik-art <sergijsemiruncik@gmail.com>
2026-02-21 19:15:51 +0200

    Homework 05 done

--- bot.py
+++ bot.py
@@ -1,26 +1,34 @@
+import functools
+
+def input_error(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (ValueError, TypeError, IndexError):
+            return "Give me name and phone number please."
+
+    return inner
+
 def parse_input(user_input: str):
     cmd, *args = user_input.strip().lower().split()
     return cmd, args
 
-
+@input_error
 def add_contact(args, contacts):
-    if len(args) != 2:
-        return "Please enter name and phone number."
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
-
+@input_error
 def change_contact(args, contacts):
-    if len(args) != 2:
-        return "Please enter name and phone number."
     name, phone = args
     if name not in contacts:
         return "Contact not found."
     contacts[name] = phone
     return "Contact updated."
 
-
+@input_error
 def show_phone(args, contacts):
     if len(args) != 1:
         return "Please enter a username."
@@ -29,7 +37,7 @@ def show_phone(args, contacts):
         return "Contact not found."
     return contacts[name]
 
-
+@input_error
 def show_all(contacts):
     if not contacts:
         return "No contacts saved."
@@ -65,10 +73,7 @@ def main():
             print("How can I help you?")
 
         elif command == "add":
-            try:
-                print(add_contact(args, contacts))
-            except ValueError:
-                print("Please enter name and phone number.")
+             print(add_contact(args, contacts))
 
         elif command == "change":
             print(change_contact(args, contacts))
